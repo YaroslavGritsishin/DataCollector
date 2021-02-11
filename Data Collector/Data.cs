d908a18fecdd9772e7b0ed7b910cc46fb498b5e2
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2021-02-11 15:50:41 +0300

    Added class for working with database

---
+++ Data Collector/Data.cs
@@ -0,0 +1,31 @@
+﻿using Data_Collector.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Collector
+{
+    public abstract class Data
+    {
+        public abstract List<PointCoordinate> GetData();
+        public void SavePoints(List<PointCoordinate> points)
+        {
+            using (Context context = new Context())
+            {
+                context.pointCoordinates.AddRange(points);
+                context.SaveChanges();
+            }
+        }
+        public void SaveBasePoint(List<BasePointCoordinate> basePoints)
+        {
+            using (Context context = new Context())
+            {
+                context.basePointCoordinates.AddRange(basePoints);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ Data Collector/Excel.cs
@@ -0,0 +1,21 @@
+﻿using Data_Collector.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Collector
+{
+    public class Excel : Data
+    {
+        public override List<PointCoordinate> GetData()
+        {
+            List<PointCoordinate> result = new List<PointCoordinate>();
+
+
+
+            return result;
+        }
+    }
+}
